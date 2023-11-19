0f5fd1e5e12df0cd404584e483664a28dd2e1be5
AndradaAkki <151175047+AndradaAkki@users.noreply.github.com>
2023-11-19 12:06:05 +0200

    Add files via upload

---
+++ welcome/welcomeJs.js
@@ -0,0 +1,64 @@
+const countryCityInfo = {
+  Romania1: ["Timisoara", "Pitesti"],
+  Italy1: ["Milano", "Venice"],
+  Germany1: ["Munich", "Nuremberg"],
+  France1: ["Paris"],
+  France2: ["Paris"],
+  France3: ["Paris"],
+  France4: ["Paris"],
+  France5: ["Paris"],
+  Romania2: ["Timisoara", "Pitesti"],
+  Italy32: ["Milano", "Venice"],
+  Germany13: ["Munich", "Nuremberg"],
+  Franc23re: ["Paris"],
+  France: ["Paris"],
+  Frangr4e: ["Paris"],
+  Francbgte: ["Paris"],
+  Frantce: ["Paris"],
+  Romania: ["Timisoara", "Pitesti"],
+  Italby: ["Milano", "Venice"],
+  Germaeny: ["Munich", "Nuremberg"],
+  France: ["Paris"],
+  Frtance: ["Paris"],
+  Frfnce: ["Paris"],
+  Frawnce: ["Paris"],
+  Fragnce: ["Paris"],
+  Romafnia: ["Timisoara", "Pitesti"],
+  Italwey: ["Milano", "Venice"],
+  Germawny: ["Munich", "Nuremberg"],
+  Frangrce: ["Paris"],
+  Franwce: ["Paris"],
+  Frbance: ["Paris"],
+  Fra3ce: ["Paris"],
+  Frante: ["Paris"],
+};
+
+window.onload = function () {
+  //todo: Get all input html elements from the DOM
+
+  const countrySelection = document.querySelector("#Country"),
+    citySelection = document.querySelector("#City");
+
+  citySelection.disabled = true; // remove all options bar first
+
+  for (let country in countryCityInfo) {
+    countrySelection.options[countrySelection.options.length] = new Option(
+      country,
+      country
+    );
+  }
+  countrySelection.onchange = (e) => {
+    citySelection.disabled = false;
+
+    citySelection.length = 1; // remove all options bar first
+
+    let city = countryCityInfo[e.target.value];
+
+    for (let i = 0; i < city.length; i++) {
+      citySelection.options[citySelection.options.length] = new Option(
+        city[i],
+        city[i]
+      );
+    }
+  };
+};
